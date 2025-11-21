6e933f2b3dc759097a7d2b8f3df73b8772c3b2e9
Shin417 <shintaroasoemail@gmail.com>
2025-11-20 20:16:53 -0500

    Added comment to Entry component

--- travel-journal/src/components/Entry.jsx
+++ travel-journal/src/components/Entry.jsx
@@ -4,8 +4,10 @@
 //3.country --> ex.) Japan
 //4.mapUrl --> google map url
 //5.date --> ex.) 12 Jan 2021 - 24 Jan 2021
-//6.description --> ex.) Mount Fuji is the tallest mountain in Japan..... 
-export default function(props){
+//6.description --> ex.) Mount Fuji is the tallest mountain in Japan.....
+
+//function to return Entry component with props
+export default function Entry(props){
     return(
         <div className="entry-container">
             <div className="entry-img-container">
