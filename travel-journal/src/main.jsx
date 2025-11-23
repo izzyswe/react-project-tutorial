27bef0040abf50f642732f15265291aeb8ffd17a
LaurisA <200595782@student.georgianc.on.ca>
2025-11-23 13:57:51 -0600

    Css Styling

--- travel-journal/src/main.jsx
+++ travel-journal/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
 
 //this is the main entry point of creating a react project
